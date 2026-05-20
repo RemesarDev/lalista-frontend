d44c6d1c02d96ac9593f59d2cffce158b975653c
Juanma <remesarjuanprogr@gmail.com>
2026-05-20 19:33:12 +0000

    feat: esqueleto funcional bruto, conexion a supabase y enlace con vercel

---
+++ app/mi-lista/page.tsx
@@ -0,0 +1,8 @@
+// Ejemplo para app/buscar/page.tsx
+export default function BuscarPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Búsqueda de Productos</h1>
+    </main>
+  );
+}

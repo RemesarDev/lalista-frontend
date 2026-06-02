1aa281a9005601bbff12e1dd816252ca5dfc8c0d
RemesarDev <remesarjuanprogr@gmail.com>
2026-06-02 16:54:33 -0300

    feat/honoEnBusquedav.2

--- app/(main)/buscar/_types/index.ts
+++ app/(main)/buscar/_types/index.ts
@@ -1,10 +1,16 @@
+// app/_types.ts
+export interface Precio {
+  cadena: string;
+  direccion: string;
+  precio: number;
+}
+
 export interface Producto {
-  id: string; 
+  id: string; // O number, según tu DB
   nombre: string;
-  precios: { 
-    nombre: string; 
-    precio: number 
-  }[];
-  precioMin: number;
-  superMasBarato: string;
+  precios: Precio[];
+}
+
+export interface BusquedaResponse {
+  productos: Producto[];
 }

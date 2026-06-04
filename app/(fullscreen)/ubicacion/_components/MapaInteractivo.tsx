b416e1bb11293cb63dca3a7030326383909220a8
RemesarDev <remesarjuanprogr@gmail.com>
2026-06-04 17:20:20 -0300

    Refactor Map component to use environment variable for map ID

--- app/(fullscreen)/ubicacion/_components/MapaInteractivo.tsx
+++ app/(fullscreen)/ubicacion/_components/MapaInteractivo.tsx
@@ -25,26 +25,23 @@ export default function MapaInteractivo({
     <div className="absolute inset-0 w-full h-full z-0 select-none">
       <Map
         defaultCenter={coordenadas}
-        //center={coordenadas} // Asegura que el mapa se mueva cuando cambian las coordenadas externamente
         zoom={zoom}
-        disableDefaultUI={true} 
-        mapId="DEMO_MAP_ID" 
-        onCameraChanged={(ev) => {
-          setZoom(ev.detail.zoom);
-        }}
-        gestureHandling={'greedy'} 
+        disableDefaultUI={true}
+        mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID}
+        onCameraChanged={(ev) => setZoom(ev.detail.zoom)}
+        gestureHandling={'greedy'}
         className="w-full h-full"
-      />
-
-      {/* PIN AVANZADO */}
-      <AdvancedMarker position={coordenadas}>
-        <div className="flex flex-col items-center pointer-events-none -translate-y-8">
-          <div className="bg-slate-900 text-white text-[10px] font-bold px-2 py-1 rounded-md shadow-md mb-1 whitespace-nowrap">
-            {radio} Km a la redonda
+      >
+        {/* ADENTRO DEL MAP */}
+        <AdvancedMarker position={coordenadas}>
+          <div className="flex flex-col items-center pointer-events-none -translate-y-8">
+            <div className="bg-slate-900 text-white text-[10px] font-bold px-2 py-1 rounded-md shadow-md mb-1 whitespace-nowrap">
+              {radio} Km a la redonda
+            </div>
+            <MapPinIcon weight="fill" className="text-red-500 text-4xl drop-shadow-md" />
           </div>
-          <MapPinIcon weight="fill" className="text-red-500 text-4xl drop-shadow-md" />
-        </div>
-      </AdvancedMarker>
+        </AdvancedMarker>
+      </Map>
     </div>
   );
 }

1fbe32a6a3dd8743ba5db5ec1dfaba8863b57800
HernandezDev <128203138+HernandezDev@users.noreply.github.com>
2026-07-03 05:53:20 +0000

    feat(auth): integrar cliente de autenticación y mejorar la gestión de usuario

--- app/_lib/auth-client.ts
+++ app/_lib/auth-client.ts
@@ -1,4 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { AuthType } from "@/app/_lib/auth";
 
 export const authClient = createAuthClient({
+    plugins: [
+    inferAdditionalFields<AuthType>()
+  ],
 });
+export type User = typeof authClient.$Infer.Session.user;
